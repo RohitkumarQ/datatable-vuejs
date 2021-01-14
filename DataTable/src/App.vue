d93dbba5215fa3ba00d97232365a20f1339fecf1
RohitkumarQ <rohit.kumar@quventix.com>
2021-01-14 16:11:00 +0530

    vue datatable

---
+++ DataTable/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <datatable />
+  </div>
+</template>
+
+<script>
+import datatable from './components/datatable'
+
+export default {
+  name: 'app',
+  components: {
+    datatable
+  }
+}
+</script>
