d4fa1bceee0e31c809f74919c19880150087969f
Shubham <shubham.narula@quventix.com>
2021-01-15 18:23:44 +0530

    modification

--- DataTable/src/components/datatable.vue
+++ DataTable/src/components/datatable.vue
@@ -1,72 +1,80 @@
-
 <template>
   <div class="container">
-      <v-client-table 
-          :data="tableData"
-          :columns="columns" 
-          :options="options">
-      </v-client-table>
+    <v-client-table :data="tableData" :columns="columns" :options="options">
+      <span slot="action" slot-scope="{ row }">
+        <button v-on:click="view(row._id)" class="mr-3">
+          <font-awesome-icon icon="eye" />
+        </button>
+        <button>
+          <font-awesome-icon icon="trash" />
+        </button>
+      </span>
+    </v-client-table>
   </div>
 </template>
 
 <script>
-
 export default {
-    data() {
-        return {
-            columns: ['first_name', 'last_name', 'email','phone_number'],
-            tableData: [],
-            options: {
-                headings: {
-                    id: 'ID',
-                    name: 'Song Name',
-                    album: 'Album'
-                },
-                sortable: ['first_name', 'last_name','email','phone_number'],
-                filterable: ['first_name', 'last_name','email','phone_number']
-            }
-        }
-    },
-    mounted() {
-        this.axios.post('http://159.65.158.18:3000/customers/allcustomers').then(res => {
-            this.tableData = res.data.customers;
-            console.log(this.tableData)
-
-        })
+  data() {
+    return {
+      columns: ["first_name", "last_name", "email", "phone_number", "action"],
+      tableData: [],
+      options: {
+        headings: {
+          id: "ID",
+          name: "Song Name",
+          album: "Album",
+          actiov: "Action",
+        },
+        sortable: ["first_name", "last_name", "email", "phone_number"],
+        filterable: ["first_name", "last_name", "email", "phone_number"],
+      },
+    };
+  },
+  mounted() {
+    this.axios
+      .post("http://159.65.158.18:3000/customers/allcustomers")
+      .then((res) => {
+        this.tableData = res.data.customers;
+        console.log(this.tableData);
+      });
+  },
+  methods:{
+    view(id){
+      this.axios
+      .get(`http://159.65.158.18:3000/adminuser/getcustomer/${id}`)
+      .then((res) => {
+        this.tableData = res.data.data;
+        console.log(this.tableData);
+      });
     }
- 
-}
+  }
+};
 </script>
 
 <style>
 #app {
   width: 95%;
-  margin-top: 50px; 
+  margin-top: 50px;
 }
-
 .VuePagination {
   text-align: center;
 }
-
 .vue-title {
   text-align: center;
   margin-bottom: 10px;
 }
-
 .vue-pagination-ad {
   text-align: center;
 }
-
 .glyphicon.glyphicon-eye-open {
   width: 16px;
   display: block;
   margin: 0 auto;
 }
-
 th:nth-child(3) {
   text-align: center;
 }
-
 .VueTables__child-row-toggler {
   width: 16px;
   height: 16px;
@@ -75,13 +83,10 @@ th:nth-child(3) {
   margin: auto;
   text-align: center;
 }
-
 .VueTables__child-row-toggler--closed::before {
   content: "+";
 }
-
 .VueTables__child-row-toggler--open::before {
   content: "-";
 }
-
 </style>
--- DataTable/src/main.js
+++ DataTable/src/main.js
@@ -1,8 +1,17 @@
+// main.js
+
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import { ClientTable } from 'vue-tables-2';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEye,faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faEye,faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(ClientTable);
 
@@ -13,5 +22,5 @@ Vue.config.productionTip = false
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 new Vue({
-    render: h => h(App)
+  render: h => h(App)
 }).$mount('#app')
